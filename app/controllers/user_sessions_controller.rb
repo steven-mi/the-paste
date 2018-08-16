0fc8b214d3d98f3e2ff1508e8d8450e4618c3690
tilmanv2 <moeller.tilman@gmail.com>
2018-08-16 12:15:38 +0200

    Add UserSession view

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user_session = UserSession.new(user_session_params)
+    @user_session = UserSession.new(user_session_params.to_h)
     if @user_session.save
       flash[:success] = "Welcome back!"
       redirect_to root_path
