1f49fcfc06d411eb31897b12393797e6843e20bd
tilmanv2 <moeller.tilman@gmail.com>
2018-08-21 13:38:34 +0200

    Add controller projects

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,20 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :projects
+
   # default route
   root 'static_pages#home'
+
+  # user session routes
 
   get 'user_sessions/new'
 
   get 'user_sessions/create'
 
   get 'user_sessions/destroy'
+
+  # user routes
 
   get 'users/new'
 
@@ -24,11 +30,25 @@ Rails.application.routes.draw do
 
   get 'users/update'
 
+  # project routes
+
+  get 'projects/index'
+
+  get 'projects/show'
+  
+  get 'projects/new'
+  
+  get 'projects/edit'
+  
+  get 'projects/create'
+
+  # static pages routes
+
   get '/home', to: 'static_pages#home'
 
   get '/help', to: 'static_pages#help'
 
-  # authlogic authentication
+  # authlogic routes
   get '/sign_up', to: 'users#new', as: :sign_up
 
   get '/sign_in', to: 'user_sessions#new', as: :sign_in
