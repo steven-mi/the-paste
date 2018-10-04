15e5e79196793915e9b011185f2a1e053528a0f9
Oriba46 <oliver-kuetemeier123@web.de>
2018-10-04 08:41:36 +0200

    Changed empty password to nil in creation, updated view for password/nopassword, updated db schema for new Project params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,7 +8,9 @@ class ProjectsController < ApplicationController
 
     @project = Project.find(params[:id])
     p "------------------SHOW------------------"
+    p "Database:" 
     p @project.password
+    p "Input:" 
     p request.GET[:password]
 
     if request.GET[:password] != @project.password
@@ -26,8 +28,13 @@ class ProjectsController < ApplicationController
   end
 
   def create
+   
+    if project_params[:password]==""
+      @project = Project.new( {title: project_params[:title],password: nil})
+    
+  else 
     @project = Project.new(project_params)
-
+  end
     if @project.save
       redirect_to @project
     else
@@ -54,7 +61,9 @@ class ProjectsController < ApplicationController
   private
 
   def project_params
-    params.require(:project).permit(:title, :password)
+
+      params.require(:project).permit(:title, :password)
+    
   end
   
 end
